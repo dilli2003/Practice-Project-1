50e7e5c11e86684e00d3c6f3e684a53af89376f1
Dilli kumar V <85548273+dilli2003@users.noreply.github.com>
2024-11-13 12:30:09 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+console.log("hello world");
+let a ;
+a = 5;
+let bookname;
+bookname = "Dilli"
+console.log(a);
+a = bookname;
+console.log(bookname);
+console.log(a);
+let name;
+name = "dilli";
+console.log(name);
